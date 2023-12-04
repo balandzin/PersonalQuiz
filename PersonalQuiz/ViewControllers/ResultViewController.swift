70ec20a0dcd7c0ee8832967ca881065f8f9978fb
balandzin <balandzin2602@gmail.com>
2023-12-04 19:42:15 +0300

    finish task

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,13 +8,45 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answersChosen: [Answer]!
+    
+    private var results: [Animal : Int] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        
+        checkResult(for: answersChosen)
     }
     
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
+    }
+    
+    private func checkResult(for answers: [Answer]) {
+        var resultCount = 1
+        for answer in answers {
+            if !results.keys.contains(answer.animal) {
+                results[answer.animal] = resultCount
+            } else {
+                resultCount += 1
+                results[answer.animal] = resultCount
+            }
+        }
+        
+        let sortedResult = results.sorted{ $0.value > $1.value }
+        guard let result = sortedResult.first?.key else { return }
+        
+        establishResult(from: result)
+    }
+    
+    private func establishResult(from result: Animal) {
+        resultLabel.text = "Вы - \(result.rawValue)"
+        descriptionLabel.text = result.definition
     }
 }
